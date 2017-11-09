3b3528f9877e0ca63e055ee7ce5098fb14077a23
Stefan (PS4Stefan) <stefan.bijen@gmail.com>
2017-11-09 23:40:47 +0100

    Allow global functions for easy access

--- Global.h
+++ Global.h
@@ -9,6 +9,20 @@
 #define AK_ARGUMENT_SIZE 64 // Max number of arguments
 #define AK_PI 3.14159265358979323846 // Define PI
 
-#define AK_FUNCTION(rFunction) Variable* rFunction(Instance)
+#define AK_FUNCTION(rFunction) Variable* rFunction(ArgParser* rParser, ArgumentData* rArgs)
+#define AK_FUNCTION_CONSTRUCTOR(rFunction) void* rFunction(ArgParser* rParser, ArgumentData* rArgs, Variable* rVar)
+#define AK_FUNCTION_DESTRUCTOR(rFunction) void rFunction(ArgParser* rParser, void* rData)
+#define AK_FUNCTION_TO_INDEX(rOperator) (int)rOperator - (int)AO_START - 1
+
+// Reference Counter (Debugging)
+#define AK_REFERENCE_INCREASE(rVar) (rVar)->IncreaseReferenceCounter()
+#define AK_REFERENCE_DECREASE(rVar) (rVar)->DecreaseReferenceCounter()
+
+// Easy access to functions
+typedef Variable* (*AKFunction)(ArgParser* rParser, ArgumentData* rArgs);
+typedef void* (*AKFunctionConstructor)(ArgParser* rParser, ArgumentData* rArgs, Variable* rVar);
+typedef void(*AKFunctionDeconstructor)(ArgParser* rParser, void* mData);
+typedef Variable* (*AKGetFunction)(ArgParser* rParser, void* rPointer);
+typedef void(*AKSetFunction)(ArgParser* rParser, void* rPointer, Variable* rVar);
 
 #endif // GLOBAL_H
